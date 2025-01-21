f408694479581c07019932d72d6e4022a460f025
husknet <127146284+husknet@users.noreply.github.com>
2025-01-21 07:53:42 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,29 +3,9 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Preprocess enables SCSS, TypeScript, PostCSS, etc.
   preprocess: preprocess(),
-
   kit: {
-    // Adapter auto detects the deployment environment and selects the best adapter.
-    adapter: adapter(),
-
-    // Files used in the app structure
-    files: {
-      assets: 'static', // Static assets like images
-      routes: 'src/routes', // Routes folder
-      appTemplate: 'src/app.html' // HTML entry point
-    },
-
-    // Vite configuration
-    vite: {
-      server: {
-        fs: {
-          // Allow serving files outside of the current project root
-          allow: ['..']
-        }
-      }
-    }
+    adapter: adapter()
   }
 };
 
